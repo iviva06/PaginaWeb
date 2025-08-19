85b69a92cc56744cad5f705abb88a7d93078c449
COMPUDEIVI\ivann <ivannavalle0606@gmail.com>
2025-08-19 17:19:21 -0300

    Creación de Register.vue

---
+++ src/components/Register.vue
@@ -0,0 +1,88 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="Register">
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Login',
+  // You can add data, methods, and other component options here
+};
+</script>
+
+<style scoped>
+/* Scoped styles ensure that the CSS only applies to this component*/
+.login {
+  background-color: white;
+  text-align: center;
+  padding: 1.5rem;
+  width: auto;
+  max-width: 90vw;
+  border-radius: 10px;
+  box-sizing: border-box;
+  box-shadow: 0 4px 15px rgba(0, 0, 0, 0.2);
+}
+
+.login img {
+  display: block;
+  max-width: 80%;
+  height: auto;
+  margin: 0 auto 0.2rem;
+  border-radius: 8px;
+}
+
+.login form {
+  padding: 2px;
+}
+
+.login form input {
+  padding: 10px;
+  font-size: large;
+}
+
+.login form .user {
+  padding: 10px;
+  font-size: xx-large;
+
+}
+
+.login form .password {
+  padding: 10px;
+  font-size: xx-large;
+}
+
+.login button {
+  margin-top: 1rem;
+  padding: 0.5rem 1rem;
+  font-size: 1.5rem;
+  background-color: #890f16;
+  border-radius: 10px;
+  border: none;
+  cursor: pointer;
+  color: white;
+  margin-right: 1rem;
+}
+
+.login button:hover {
+  background-color: #890f16;
+}
+
+@media (max-width: 480px) {
+  .login {
+    padding: 1rem;
+    font-size: 0.9rem;
+  }
+
+  .login form {
+    font-size: 1rem;
+  }
+}
+
+@media (max-width: 768px) {
+  .login {
+    width: 95%;
+  }
+}
+</style>
