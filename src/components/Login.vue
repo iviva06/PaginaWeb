4d3a5b88c82e08b402484bf9f5b0c7f138cb8a59
COMPUDEIVI\ivann <ivannavalle0606@gmail.com>
2025-08-19 10:19:39 -0300

    Eliminación de comentario innecesario en Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -26,7 +26,7 @@ export default {
 </script>
 
 <style scoped>
-/* Scoped styles ensure that the CSS only applies to this component vhvhvmjngv*/
+/* Scoped styles ensure that the CSS only applies to this component*/
 .login {
   background-color: white;
   text-align: center;
