aeeee2c4c83dfa4d3f5a7d526ec2ee4a827fa2dd
Juan Ignacio Marco <juani.marco16@gmail.com>
2025-08-25 20:10:09 -0300

    Creacion de AgregarCurso y AgregarEstudiantes

--- src/components/HomePrincipal.vue
+++ src/components/HomePrincipal.vue
@@ -1,16 +1,160 @@
 <template>
-  <SideBar>
+  <header>
+    <img src="../assets/logo_header.jpg" alt="Logo Circular UB" />
+    <h1>Universidad de Belgrano</h1>
+
+    <div style="margin-left: auto" class="logoutButton">
+      <router-link to="/">
+        <button>Cerrar sesión <i class="bx bxs-door-open"></i></button>
+      </router-link>
+    </div>
+  </header>
+
+  <div class="sidebar">
+    <aside>
+      <router-link to="/agregarestudiante">
+        <button><i class="bx bxs-user"></i> Agregar estudiante</button>
+      </router-link>
+      <router-link to="/agregarcurso">
+        <button class="addCourseButton"><i class="bx bxs-book"></i> Agregar un curso</button>
+      </router-link>
+      <select>
+        <option>Programación</option>
+        <option>Matemáticas</option>
+        <option>Física</option>
+      </select>
+    </aside>
     <main>
       <h2>Bienvenido/a a la Gestión de Cursos</h2>
       <p>Elija la opción que desee</p>
     </main>
-  </SideBar>
+  </div>
+
+  <main class="page-content">
+    <slot></slot>
+  </main>
 </template>
 
-<script setup>
-import SideBar from './SideBar.vue';
-</script>
+<script setup></script>
 
 <style scoped>
+* {
+  box-sizing: border-box;
+}
 
+/*---------Header---------*/
+
+header button {
+  background-color: #6f1515;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+  border-radius: 16px;
+  font-family: questrial, sans-serif;
+}
+
+.logoutButton button {
+  display: flex;
+  align-items: center;
+  gap: 3px;
+}
+
+.logoutButton a {
+  text-decoration: none; /* para borrar la linea azul de hipervinculo */
+}
+
+header {
+  background-color: #6f1515; /* rojo UB */
+  color: white;
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande", "Lucida Sans Unicode", Geneva,
+    Verdana, sans-serif;
+}
+
+header img {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-right: 15px;
+}
+
+/*---------Sidebar---------*/
+
+.sidebar {
+  display: flex;
+}
+aside {
+  background-color: #d3d3d3;
+  margin: 0;
+  padding: 0;
+  width: 220px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  height: 100vh;
+}
+
+aside button {
+  background-color: #d3d3d3;
+  color: black;
+  border: 1px solid #aaa;
+  padding: 10px;
+  cursor: pointer;
+  text-align: left;
+  display: flex;
+  align-items: center;
+  gap: 7px;
+  border-radius: 16px;
+  font-family: questrial, sans-serif;
+}
+
+aside select {
+  padding: 10px;
+  border: 1px solid #464141;
+  background-color: rgba(123, 115, 115, 0.226);
+  font-family: questrial, sans-serif;
+  border-radius: 16px;
+}
+
+.login form {
+  padding: 2px;
+}
+
+form .ID {
+  padding: 10px;
+  font-size: x-large;
+  margin-bottom: 10px;
+}
+main {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 20x;
+  font-size: xx-large;
+  color: rgb(0, 0, 0);
+  font-family: questrial, sans-serif;
+}
+
+.saveButton button {
+  margin-top: 1rem;
+  padding: 0.5rem 1rem;
+  font-size: 1.5rem;
+  background-color: #890f16;
+  border-radius: 10px;
+  border: none;
+  cursor: pointer;
+  color: white;
+  margin-right: 1rem;
+}
+main h2 {
+  font-size: xxx-large;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,47 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../components/Login.vue'
-import Register from '../components/Register.vue'
-import SideBar from '../components/SideBar.vue'
-import HomePrincipal from '../components/HomePrincipal.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "../components/Login.vue";
+import Register from "../components/Register.vue";
+import SideBar from "../components/SideBar.vue";
+import HomePrincipal from "../components/HomePrincipal.vue";
+import AgregarCurso from "../components/AgregarCurso.vue";
+import AgregarEstudiante from "../components/AgregarEstudiante.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Login',
-    component: Login
+    path: "/",
+    name: "Login",
+    component: Login,
   },
   {
-    path: '/register',
-    name: 'Register',
-    component: Register
+    path: "/register",
+    name: "Register",
+    component: Register,
   },
   {
-    path: '/sidebar',
-    name: 'Sidebar',
-    component: SideBar
+    path: "/sidebar",
+    name: "Sidebar",
+    component: SideBar,
   },
   {
-    path: '/homeprincipal',
-    name: 'HomePrincipal',
-    component: HomePrincipal
-  }
-]
+    path: "/homeprincipal",
+    name: "HomePrincipal",
+    component: HomePrincipal,
+  },
+  {
+    path: "/agregarcurso",
+    name: "AgregarCurso",
+    component: AgregarCurso,
+  },
+  {
+    path: "/agregarestudiante",
+    name: "AgregarEstudiante",
+    component: AgregarEstudiante,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
