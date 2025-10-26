cb2c90408b7af11da434c0d61dec8b3d76eec420
DiegoVargasFalla <fallaandres1007f@gmail.com>
2025-10-25 23:09:57 -0300

    change base path

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
 
-  base: mode === "development" ? "/" : "/PaginaWeb/",
+  base: "/PaginaWeb/",
 
   plugins: [
     vue(),
